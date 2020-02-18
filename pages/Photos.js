c3b7c2e0c9653ecb876bd14f2183a31e30ee6186
jordanyatckoske <jordanyatckoske@gmail.com>
2020-02-18 11:33:22 -0600

    render photos from markdown

--- pages/Photos.js
+++ pages/Photos.js
@@ -1,10 +1,15 @@
 import MainLayout from "../components/Layouts/MainLayout";
+import { attributes, react as PhotoContent } from "../content/photos/photos.md";
+import PhotosList from "../components/Photos/PhotosList";
 
 export default function Photos() {
+  let { photos } = attributes;
+
   return (
     <MainLayout>
       <h2>Photos</h2>
       <p>Some content about some stuff doing the things</p>
+      <PhotosList photos={photos} />
     </MainLayout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,19 @@
-import Header from "../components/Header";
 import MainLayout from "../components/Layouts/MainLayout";
+import PressList from "../components/Press/PressList";
 import Head from "next/head";
-import { Component } from "react";
-import { attributes, react as HomeContent } from "../content/home.md";
+import { attributes, react as QuoteContent } from "../content/quotes/quotes.md";
 
 export default function Index() {
-  let { title, cats } = attributes;
+  let { pressQuotes } = attributes;
+
+  let homeQuotes = pressQuotes.filter(quote => quote.featuredOnHome === true);
   return (
     <MainLayout>
       <p>Hello Next.js</p>
       <Head>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
-      <article>
-        <h1>{title}</h1>
-        <HomeContent />
-        <ul>
-          {cats.map((cat, k) => (
-            <li key={k}>
-              <h2>{cat.name}</h2>
-              <p>{cat.description}</p>
-            </li>
-          ))}
-        </ul>
-      </article>
+      <PressList pressQuotes={homeQuotes} />
     </MainLayout>
   );
 }
