ecd995126da387fdc4c9b4e31796c6d258796498
jordanyatckoske <jordanyatckoske@gmail.com>
2020-03-02 11:32:46 -0600

    update artist info for bio and artist name

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import MainLayout from "../components/Layouts/MainLayout";
 import PressList from "../components/Press/PressList";
 import Head from "next/head";
-// import {
-//   attributes as quoteAttributes,
-//   react as QuoteContent,
-// } from "../content/quotes/quotes.md";
 import {
-  attributes as bioAttributes,
-  react as BioContent,
-} from "../content/personalInfo/natalie-fideler.md";
+  attributes as quoteAttributes,
+  react as QuoteContent,
+} from "../content/quotes/press.md";
+// import {
+//   attributes as bioAttributes,
+//   react as BioContent,
+// } from "../content/personalInfo/natalie-fideler.md";
 
 export default function Index() {
-  // let { pressQuotes } = quoteAttributes;
-  let { artistName } = bioAttributes;
+  let { pressQuotes } = quoteAttributes;
+  // let { artistName } = bioAttributes;
 
-  // let homeQuotes = pressQuotes.filter(quote => quote.featuredOnHome === true);
+  let homeQuotes = pressQuotes.filter(quote => quote.featuredOnHome === true);
   return (
     <MainLayout>
       <Head>
@@ -22,10 +22,10 @@ export default function Index() {
       </Head>
       <div className="hero">
         <div className="hero-body">
-          <h1 className="title is-1">{artistName}</h1>
+          {/* <h1 className="title is-1">{artistName}</h1> */}
         </div>
       </div>
-      {/* <PressList pressQuotes={homeQuotes} /> */}
+      <PressList pressQuotes={homeQuotes} />
     </MainLayout>
   );
 }
